1c2e2a462b7387fff349743204f38bac46fe6891
YiFan <jiao1995cn@gmail.com>
2023-03-03 13:44:20 -0600

    phone number validated created

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,6 +8,16 @@ const outputPpo = document.querySelector('.p-po');
 const inputEmail = document.querySelector('.email');
 const outputEpo = document.querySelector('.e-po');
 
+inputPhone.addEventListener('mouseout', () => {
+    const verify = /^\d{10}$/;
+    if (!verify.test(inputPhone.value.trim()) || inputPhone.value.trim() === '' ) {
+        outputPpo.style.visibility = 'visible';
+        outputPpo.innerHTML = 'Please enter the correct phone number'
+    } else {
+        outputPpo.style.visibility = 'hidden';
+    }
+});
+
 inputEmail.addEventListener('mouseout', () => {
     const verify = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
     if (!verify.test(inputEmail.value.trim()) || inputEmail.value.trim() === '' ) {
