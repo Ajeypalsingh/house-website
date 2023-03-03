d0eded403801958edef66056aa448cf52681b289
YiFan <jiao1995cn@gmail.com>
2023-03-03 13:53:03 -0600

    name validated created

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,12 +1,26 @@
 'use strict'
 
 const inputUser = document.querySelector('.user');
+const outputUpo = document.querySelector('.u-po');
 
 const inputPhone = document.querySelector('.phone');
 const outputPpo = document.querySelector('.p-po');
 
 const inputEmail = document.querySelector('.email');
 const outputEpo = document.querySelector('.e-po');
+
+const inputMessage = document.querySelector('.message');
+const outputMpo = document.querySelector('.m-po');
+
+inputUser.addEventListener('mouseout', () => {
+    const verify = /^([A-Za-z]+\s?)*[A-Za-z]$/;
+    if (!verify.test(inputUser.value.trim()) || inputUser.value.trim() === '' ) {
+        outputUpo.style.visibility = 'visible';
+        outputUpo.innerHTML = 'Please enter the correct name'
+    } else {
+        outputUpo.style.visibility = 'hidden';
+    }
+});
 
 inputPhone.addEventListener('mouseout', () => {
     const verify = /^\d{10}$/;
@@ -25,5 +39,15 @@ inputEmail.addEventListener('mouseout', () => {
         outputEpo.innerHTML = 'Please enter the correct mailbox'
     } else {
         outputEpo.style.visibility = 'hidden';
+    }
+});
+
+inputMessage.addEventListener('mouseout', () => {
+    const verify = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    if (inputMessage.value.trim() === '' ) {
+        outputMpo.style.visibility = 'visible';
+        outputMpo.innerHTML = 'The message cannot be empty'
+    } else {
+        outputMpo.style.visibility = 'hidden';
     }
 });
